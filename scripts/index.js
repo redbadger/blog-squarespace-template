a592e99b787b49e3bc6dcc076b09708526b93fbd
Alex Savin <karismafilms@gmail.com>
2017-01-19 16:50:51 +0000

    Fix for redbadger/website-honestly#326 making page non-scrollable when mobile menu is open

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,12 @@
 document.getElementById("smallScreenNavMargin").addEventListener("click", function(){
-  document.getElementById('burger').click()
+  document.getElementById('burger').click();
+  document.getElementsByTagName('html')[0].setAttribute('style', 'overflow-y: auto');
+});
+
+document.getElementById("openBurger").addEventListener("click", function(){
+  document.getElementsByTagName('html')[0].setAttribute('style', 'overflow-y: hidden');
+});
+
+document.getElementById("closeBurger").addEventListener("click", function(){
+  document.getElementsByTagName('html')[0].setAttribute('style', 'overflow-y: auto');
 });
