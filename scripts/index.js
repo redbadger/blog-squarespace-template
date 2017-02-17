1949355560043070f56dd730a069fa2ab05bc77c
Alexander Savin <asavin@users.noreply.github.com>
2017-02-17 15:07:16 +0000

    Open page (background content) shouldn't be scrollable when mobile nav is open (#46)
    * Fix for redbadger/website-honestly#326 making page non-scrollable when mobile menu is open
    
    * Code style fix to use single quotes in js
    
    * fix body scrolling when mobile menu is open
    
    * keep background scrolling disabled after going back to the blog on mobile

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,32 @@
-document.getElementById("smallScreenNavMargin").addEventListener("click", function(){
-  document.getElementById('burger').click()
+function scrollLock() {
+  document.body.style.position = "fixed";
+}
+
+function scrollRelease() {
+  document.body.style.position = "static";
+}
+
+if (document.getElementById("smallScreenNavWrapper").offsetParent !== null) {
+  scrollLock();
+}
+
+document.getElementById("smallScreenNavMargin").addEventListener("click", function() {
+  if (document.body.style.position === "fixed") {
+    scrollRelease();
+  } else {
+    scrollLock();
+  }
+  document.getElementById('burger').click();
+});
+
+document.getElementById("menuOpenTrigger").addEventListener("click", function() {
+  scrollLock();
+});
+
+document.getElementById("menuCloseTrigger").addEventListener("click", function() {
+  scrollRelease();
+});
+
+document.getElementById("burger").addEventListener("click", function() {
+  document.getElementById('smallScreenNavWrapper').scrollTop = 0;
 });
