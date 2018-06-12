1baffbdc635432bd44927fb0c5871742939e2657
ajcumine <ajcumine@gmail.com>
2018-06-12 14:04:16 +0100

    split tracking across page types, add referral tracking

--- scripts/amplitude.js
+++ scripts/amplitude.js
@@ -10,27 +10,46 @@ function getBlogInfo () {
     blogTitle,
     author,
     categories,
+    pageType: 'blog',
   }
 }
 
 function logAmplitudeEvent (eventType, eventProperties, test = false) {
+  var blogInfo = document.getElementsByClassName('blog-article').length > 0 ? getBlogInfo() : { pageType: 'blogLanding' };
+  var query = window.location.search.substr(1);
+
+  var referrerProperties = { referrer: 'internal' };
+  if (query.indexOf('utm_source') >= 0) {
+    var utmProperties = {};
+    query.split("&").forEach(function(part) {
+      var item = part.split("=");
+      utmProperties[item[0]] = decodeURIComponent(item[1]);
+    });
+
+    referrerProperties = {
+      referrer: utmProperties.utm_source,
+      utmContent: utmProperties.utm_content,
+      utmMedium: utmProperties.utm_medium,
+    };
+  }
+
+  var urlProperties = {
+    ...referrerProperties,
+    url: window.location.href,
+  };
+
   var customProperties = {
     ...eventProperties,
-    ...getBlogInfo(),
-    url: window.location.href,
-  }
+    ...blogInfo,
+    ...urlProperties,
+  };
 
   if (test == true) {
     console.log(eventType, customProperties);
   } else {
     amplitude.getInstance().logEvent(eventType, customProperties);
-  }  
+  }
 };
-
-document.addEventListener('DOMContentLoaded', function () {
-  logAmplitudeEvent('PAGE LOADED', { pageType: 'blog' });
-  logAmplitudeEvent('SCROLL', { scrollPercentage: 0 }, true);
-});
 
 function logSocialShareClick (socialNetwork, subject) {
   return function () {
@@ -41,23 +60,6 @@ function logSocialShareClick (socialNetwork, subject) {
     );
   }
 }
-
-var socialLinks = document.getElementsByClassName('social-links__icon');
-
-for (var i = 0; i < socialLinks.length; i++) {
-  var socialNetwork = socialLinks[i].children[0].alt;
-  var linkLocation = socialLinks[i].parentElement.parentElement.parentElement.parentElement.className === 'meta-block' ? 'sidebar' : 'footer';
-
-  socialLinks[i].addEventListener('click', logSocialShareClick(socialNetwork, linkLocation));
-}
-
-document.getElementsByClassName('site-footer__telLink')[0].addEventListener('click', function () {
-  logAmplitudeEvent('CLICK CONTACT US', {type: 'phone', subject: 'footer'});
-});
-
-document.getElementsByClassName('site-footer__mailtoLink')[0].addEventListener('click', function () {
-  logAmplitudeEvent('CLICK CONTACT US', {type: 'email', subject: 'footer'});
-});
 
 var log100ScrollDepth = function () {
   var scrollPercentage = Math.round((window.scrollY / (document.documentElement.scrollHeight - window.innerHeight))* 100);
@@ -98,4 +100,37 @@ var log25ScrollDepth = function () {
   }
 };
 
-window.addEventListener('scroll', log25ScrollDepth);
+// All pages tracking
+
+document.getElementsByClassName('site-footer__telLink')[0].addEventListener('click', function () {
+  logAmplitudeEvent('CLICK CONTACT US', {type: 'phone', subject: 'footer'});
+});
+
+document.getElementsByClassName('site-footer__mailtoLink')[0].addEventListener('click', function () {
+  logAmplitudeEvent('CLICK CONTACT US', {type: 'email', subject: 'footer'});
+});
+
+if (document.getElementsByClassName('blog-article').length > 0) {
+
+  // Blog page tracking
+  document.addEventListener('DOMContentLoaded', function () {
+    logAmplitudeEvent('PAGE LOADED');
+    logAmplitudeEvent('SCROLL', { scrollPercentage: 0 });
+  });
+
+  var socialLinks = document.getElementsByClassName('social-links__icon');
+  for (var i = 0; i < socialLinks.length; i++) {
+    var socialNetwork = socialLinks[i].children[0].alt;
+    var linkLocation = socialLinks[i].parentElement.parentElement.parentElement.parentElement.className === 'meta-block' ? 'sidebar' : 'contentFooter';
+
+    socialLinks[i].addEventListener('click', logSocialShareClick(socialNetwork, linkLocation));
+  }
+
+  window.addEventListener('scroll', log25ScrollDepth);
+} else {
+
+  // Landing page tracking
+  document.addEventListener('DOMContentLoaded', function () {
+    logAmplitudeEvent('PAGE LOADED');
+  });
+}
